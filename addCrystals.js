74aac698f2f163dd166c435b14b976964fd899d8
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-08-14 20:54:56 +0700

    Create addCrystals.js

---
+++ addCrystals.js
@@ -0,0 +1,48 @@
+/* Don't touch this! */
+var manageAliens = function(game)
+{
+  if (!game._init)
+  {
+    game._init = true;
+    try {
+      game.modding.I1I0I.modding.game.alienCreated = function(t, e) {
+        var i, s, n, r;
+        for (r = this.aliens, s = 0, n = r.length; s < n; s++)
+        {
+          i = r[s];
+          if (i.request_id === t)
+          {
+            i.id = e;
+            let id = this.execAliens.indexOf(t);
+            if (id != -1)
+            {
+              i.set({kill:true});
+              this.execAliens.splice(id,1);
+            }
+          }
+        }
+      }
+    }
+    catch(e){game._init = false}
+  }
+}
+game.execAliens = [];
+game.addCrystal = function(data)
+{
+  this.execAliens.push(game.modding.I1I0I.request_id);
+  this.addAlien({code:13, x:data.x||0, y:data.y||0, crystal_drop: data.value||0});
+}
+
+/* Do mod stuffs below, don't remove anything in this.tick, just add */
+
+this.options = {
+  // see documentation for options reference
+  root_mode: "survival",
+  map_size: 30
+};
+
+this.tick = function(game)
+{
+  manageAliens(game);
+  // do mod stuff here, see documentation
+}
