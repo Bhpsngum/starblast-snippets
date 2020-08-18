3c2aef74421fef275fac62aacdfc97b25ec59824
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-08-18 15:33:21 +0700

    lol

--- imageUI.js
+++ imageUI.js
@@ -1,58 +1,65 @@
+/* ADD SMALL IMAGE (lower than 35x35) TO THE GAME
+Syntax:
+game.configImageUI(options, handler)
+
+options: an object with preferences:
+* id, visible, clickable and position: same as normal UI Components
+* url: the Image URL (strongly recommends Raw GitHub links)
+* opacity: opacity of the image (from 0 to 1)
+
+handler: a function to handle the results
+
+Note: image are not rendered with pixels, its height and width depend to the user's browser window's width and height, so define the postion wisely :D
+
+for example:
+game.configImageUI({ 
+  id:"example_img", 
+  visible:true, 
+  clickable:false, 
+  url:"https://raw.githubusercontent.com/Bhpsngum/img-src/master/yeetus.png", 
+  position: [50,60,10,10], 
+  opacity:1 
+},game.ships[0].setUIComponent);
+
+will add an imageUIComponent from yeetus.png to the first player's screen
+
+Note: if your image's resolution is too high (more than 35x35), the game will crashed for unknown reason (websocket sending limit maybe?)
+
+*/
 game.configImageUI = function(opt, handler)
 {
-  let image = {
-    id: opt.id,
-    visible: opt.visible,
-    clickable: opt.clickable,
-    components: []
-  };
-  if (opt.id == null) throw new Error("Invalid UI Component ID");
   if (opt.url)
   {
-    let img = $("#"+opt.id)[0];
-    if (!img)
-    {
-      img = document.createElement("img");
-      img.setAttribute("id",opt.id);
-      img.setAttribute("style","display:none");
-      img.crossOrigin = "anonymous";
-      $('body').append(img);
-    }
+    let image = {
+      id: opt.id,
+      visible: opt.visible,
+      position: opt.position,
+      clickable: opt.clickable,
+      components: []
+    }, img = new Image();
+    img.crossOrigin = "Anonymous";
     img.onload = function(){
       this.canvas = $('<canvas />')[0];
       this.canvas.width = img.width;
       this.canvas.height = img.height;
       this.canvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
-      let mx,my,h=100/this.height,w=100/this.width,d=[],pos=opt.position||{x:0,y:0},scale = opt.scale||{x:1,y:1};
+      let h=100/this.height,w=100/this.width;
       function round(num)
       {
         return parseFloat(num.toFixed(2));
       }
-      if (["scoreboard","radar_background"].indexOf(opt.id) != -1)
-      {
-        d=[0,0];
-        mx=(pos.x||0)/100;
-        my=(pos.y||0)/100;
-      }
-      else
-      {
-        d = [pos.x||0,pos.y||0];
-        mx=0;
-        my=0;
-      }
-      image.position = [d,[round(w*(scale.x||1)),round(h*(scale.y||1))]].flat();
       for (let i=0;i<this.height;i++)
       {
         for (let j=0;j<this.width;j++)
         {
           let data = img.canvas.getContext('2d').getImageData(j,i,1,1).data;
-          image.components.push({type:"box",position:[round(w*j+mx),round(h*i+my),round(w*1.5),round(h*1.5)],fill:`rgba(${data[0]},${data[1]},${data[2]},${data[3]/255*(opt.opacity||1)})`});
+          image.components.push({type:"box",position:[round(w*j),round(h*i),round(w+0.5),round(h+0.5)],fill:`rgba(${data[0]},${data[1]},${data[2]},${data[3]/255*(opt.opacity||1)})`});
         }
       }
       (typeof handler == "function") && handler(image);
     }
     img.onerror = function(){throw new Error("Failed to fetch the image")}
-    img.setAttribute("src",opt.url);
+    img.src = opt.url;
   }
   else throw new Error("No Image URL detected");
 }
