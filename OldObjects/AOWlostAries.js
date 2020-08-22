2d5f6fe8ef6f029b0dffd41bccec6312003cfe98
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-08-22 16:28:14 +0700

    Create AOWlostAries.js

---
+++ OldObjects/AOWlostAries.js
@@ -0,0 +1,18 @@
+var lost_sector_aries = {
+  id:"lost_sector_aries",
+  obj:"https://starblast.io/lost_sector/LostSector_Aries_HardEdges.obj",
+  diffuse:"https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Diffuse.jpg",
+  bump:"https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Height.jpg",
+  specular:"https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Specular.jpg",
+  shininess:10,
+  emissiveColor:0,
+  specularColor:16777215,
+  transparent:false
+}
+game.setObject({
+  id:"lost_sector_aries",
+  type: lost_sector_aries,
+  position: {x:75,y:-25,z:-60},
+  scale: {x:3,y:3,z:3},
+  rotation: {x:0,y:0,z:Math.PI}
+})
