2a73045e7bc44d954b1627930aa718b3b0d0e184
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-16 01:27:17 +0700

    more decoration

--- mods/safeZones.js
+++ mods/safeZones.js
@@ -3,12 +3,23 @@ SAFE ZONES MOD by Bhpsngum
 based on the famous traditional game Musical Chairs
 */
 
-/* Configurable values - You can edit this section while waiting for players! */
-var waiting_time = 15; // lobby waiting time when there are enough players (in seconds, non-zero - of course)
-var players = 10; // Number of players (minimum 10), One-time edit only
-var map_size = 20; // Map size (must be even, minimum 20), One-time edit only
-var round_time_max = 30; // First round duration (in seconds, minimum 30)
-var round_time_min = 10; // Last round duration (in seconds, minimum 10)
+/* Configurable values (line 9 -> line 38) */
+/* You can edit some properties in this section while waiting for other players! */
+
+var waiting_time = 15;
+// lobby waiting time when there are enough players (in seconds, non-zero - of course)
+
+var players = 10;
+// Number of players (minimum 10), One-time edit only
+
+var map_size = 20;
+// Map size (must be even, minimum 20), One-time edit only
+
+var round_time_max = 30;
+// First round duration (in seconds, minimum 30)
+
+var round_time_min = 10;
+// Last round duration (in seconds, minimum 10)
 
 var PermaSafe = "random";
 /* Permanent Safety: ships can't be knocked out or not by other players when standing in blue zones
