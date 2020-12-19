5e82c5e5dd74459fb7504a7bbf7d2d0eab31b700
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-19 23:11:17 +0700

    lmao

--- TickingWithSwitchCase.js
+++ TickingWithSwitchCase.js
@@ -1,4 +1,4 @@
-var _ = (t,...c) => ((c.length>0)?c[0]:game.step) % t === 0;
+var d = (t,...c) => ((c.length>0)?c[0]:game.step) % t;
 function doTask1() {
   echo("Now it's ten!");
 }
@@ -9,9 +9,9 @@ function doTask3() {
   echo("Awesome triple! Thirty approached! :D");
 }
 this.tick = function (game) {
-  switch (true) {
-    case _(30): doTask3();
-    case _(20): doTask2();
-    case _(10): doTask1();
+  switch (0) {
+    case d(30): doTask3();
+    case d(20): doTask2();
+    case d(10): doTask1();
   }
 }
