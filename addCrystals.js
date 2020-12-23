339e0e447730b9e4b36b6e9fa000f350e04563e5
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-24 00:32:06 +0700

    update old code lol

--- addCrystals.js
+++ addCrystals.js
@@ -4,20 +4,23 @@ var manageAliens = function(game)
   if (!game._init)
   {
     game._init = true;
-    try {
-      game.modding.I1I0I.modding.game.alienCreated = function(t, e) {
-        var i, s, n, r;
-        for (r = this.aliens, s = 0, n = r.length; s < n; s++)
-        {
-          i = r[s];
-          if (i.request_id === t)
+    for (let i in game.modding) try {
+      if (game.modding[i].modding && i != "game") {
+        game.moddingPath = i;
+        game.modding[game.moddingPath].modding.game.alienCreated = function(t, e) {
+          var i, s, n, r;
+          for (r = this.aliens, s = 0, n = r.length; s < n; s++)
           {
-            i.id = e;
-            let id = this.execAliens.indexOf(t);
-            if (id != -1)
+            i = r[s];
+            if (i.request_id === t)
             {
-              i.set({kill:true});
-              this.execAliens.splice(id,1);
+              i.id = e;
+              let id = this.execAliens.indexOf(t);
+              if (id != -1)
+              {
+                i.set({kill:true});
+                this.execAliens.splice(id,1);
+              }
             }
           }
         }
@@ -30,7 +33,7 @@ game.execAliens = [];
 game.addCrystal = function(data)
 {
   data = data || {};
-  this.execAliens.push(game.modding.I1I0I.request_id);
+  this.execAliens.push(game.modding[game.moddingPath].request_id);
   let crystal = {x:data.x||0, y:data.y||0, value: data.value||0,
     toString: function(){return "Crystal:"+JSON.stringify(this)}
   };
@@ -43,7 +46,7 @@ game.addCrystal = function(data)
 /* RESTRICTIONS - Do not use the values/variables/components listing below in their particular contexts:
 Global Scope Variables:
   manageAliens
-  
+
 Game properties:
   game.execAliens
 
