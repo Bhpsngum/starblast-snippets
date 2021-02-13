20a377964a1cf7527965763bc1fa32e987b95354
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-02-14 00:13:46 +0700

    test

--- mods/sdc.js
+++ mods/sdc.js
@@ -89,6 +89,24 @@ this.tick = function(game) {
       scale: {x:10*arena_radius,y:10*arena_radius,z:10*arena_radius}
     });
   });
+  var lost_sector_aries = {
+    id: "lost_sector_aries",
+    obj: "https://starblast.io/lost_sector/LostSector_Aries_HardEdges.obj",
+    diffuse: "https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Diffuse.jpg",
+    bump: "https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Height.jpg",
+    specular: "https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Specular.jpg",
+    shininess: 10,
+    emissiveColor: 0,
+    specularColor: 0xFFFFFF,
+    transparent: false
+  }
+  game.setObject({
+    id: "lost_sector_aries",
+    type: lost_sector_aries,
+    position: {x:0, y:0, z:-60},
+    scale: {x:3, y:3, z:3},
+    rotation: {x:0, y:0, z:Math.PI}
+  });
   this.tick = main_game;
 }
 
