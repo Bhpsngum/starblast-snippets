477dc458da140e86a90dc6a56194867dbbf7d49d
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-04-03 18:08:11 +0700

    update

--- addCrystals.js
+++ addCrystals.js
@@ -1,51 +1,12 @@
-/* Don't touch this! */
-var manageAliens = function(game)
-{
-  if (!game._init)
-  {
-    game._init = true;
-    for (let i in game.modding) try {
-      if (game.modding[i].modding && i != "game") {
-        game.moddingPath = i;
-        game.modding[game.moddingPath].modding.game.alienCreated = function(t, e) {
-          var i, s, n, r;
-          for (r = this.aliens, s = 0, n = r.length; s < n; s++)
-          {
-            i = r[s];
-            if (i.request_id === t)
-            {
-              i.id = e;
-              let id = this.execAliens.indexOf(t);
-              if (id != -1)
-              {
-                i.set({kill:true});
-                this.execAliens.splice(id,1);
-              }
-            }
-          }
-        }
-      }
-    }
-    catch(e){game._init = false}
-  }
-}
-game.execAliens = [];
-game.addCrystal = function(data)
-{
-  data = data || {};
-  this.execAliens.push(game.modding[game.moddingPath].request_id);
-  let crystal = {x:data.x||0, y:data.y||0, value: data.value||0,
-    toString: function(){return "Crystal:"+JSON.stringify(this)}
-  };
-  this.addAlien({code:13,x:crystal.x,y:crystal.y,crystal_drop:crystal.value});
-  return crystal;
-}
+/* Add a crystal drop field to the game
 
-/* Do mod stuffs below, don't remove anything in this.tick, just add */
+To create a crystal drop, use game.addCrystal({ options })
+option         value
+  x          X Coordinate
+  y          Y Coordinate
+value        Crystal value
 
-/* RESTRICTIONS - Do not use the values/variables/components listing below in their particular contexts:
-Global Scope Variables:
-  manageAliens
+RESTRICTIONS - Do not use the values/variables/components listing below in their particular contexts:
 
 Game properties:
   game.execAliens
@@ -57,10 +18,53 @@ this.options = {
   // see documentation for options reference
   root_mode: "survival",
   map_size: 30
-};
+}
 
 this.tick = function(game)
 {
-  manageAliens(game);
   // do mod stuff here, see documentation
 }
+
+/* Don't touch the part below! This must be appended at the end of your mod code! */
+;(function() {
+  var manageAliens = function (game) {
+    var alienPath = Object.keys(game.modding).find(i => {
+      try {
+        return game.modding[i].modding && i != "game"
+      }
+      catch(e) {
+        return false
+      }
+    });
+    if (!alienPath) game.modding.terminal.error("Failed to initialize 'game.addCrystal'");
+    else {
+      var x = game.modding[alienPath].alienCreated;
+      game.modding[alienPath].alienCreated = function (...args){
+        x.call(this, ...args);
+        let t = args[0].id, al = game.findAlien(t);
+        if (game.aliens.find(alien => alien.request_id === args[0].request_id) && al) {
+          let tid = game.execAliens.indexOf(t);
+          if (tid != -1) {
+            al.set({kill:true});
+            game.execAliens.splice(tid, 1);
+          }
+        }
+      }
+      game.execAliens = [];
+      game.addCrystal = function(data)
+      {
+        data = data || {};
+        let crystal = {x:data.x||0, y:data.y||0, value: data.value||0,
+          toString: function(){return "Crystal:"+JSON.stringify(this)}
+        };
+        this.execAliens.push(this.addAlien({code:13,x:crystal.x,y:crystal.y,crystal_drop:crystal.value}).request_id);
+        return crystal;
+      }
+    }
+  }, tick = this.tick;
+  this.tick = function (game) {
+    manageAliens(game);
+    this.tick = tick;
+    this.tick(game);
+  }
+}).call(this);
