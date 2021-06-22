cc4953272a35a639695cddd432aa322ff6442f52
Bhpsngum <bhpsngumproductions@gmail.com>
2021-06-22 17:12:20 +0700

    Create survival_default_vocabulary.js

---
+++ default_vocabulary/survival_default_vocabulary.js
@@ -0,0 +1,18 @@
+[
+  {"text":"Hello","icon":"\u0045","key":"O"},
+  {"text":"Bye","icon":"\u0046","key":"B"},
+  {"text":"Yes","icon":"\u004c","key":"Y"},
+  {"text":"No","icon":"\u004d","key":"N"},
+  {"text":"Thanks","icon":"\u0041","key":"X"},
+  {"text":"Sorry","icon":"\u00a1","key":"S"},
+  {"text":"No Prob","icon":"\u0047","key":"P"},
+  {"text":"Good Game","icon":"\u00470047","key":"G"},
+  {"text":"Team","icon":"\u0031","key":"T"},
+  {"text":"Follow me","icon":"\u0050","key":"F"},
+  {"text":"Mine","icon":"\u0044","key":"M"},
+  {"text":"Attack","icon":"\u0049","key":"A"},
+  {"text":"Help","icon":"\u004a","key":"H"},
+  {"text":"Kill","icon":"\u005b","key":"K"},
+  {"text":"Leader","icon":"\u002e","key":"L"},
+  {"text":"Hmm","icon":"\u004b","key":"Q"}
+]
