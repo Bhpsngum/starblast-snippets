aa9c75845aa4f9ef4f3dc60b66e54abd726a0e4b
Bhpsngum <bhpsngumproductions@gmail.com>
2021-06-22 17:31:02 +0700

    Create tutorial_default_vocabulary.js

---
+++ default_vocabulary/tutorial_default_vocabulary.js
@@ -0,0 +1,18 @@
+[
+  {"text":"Hello","icon":"\u0045","key":"L"},
+  {"text":"Bye","icon":"\u0046","key":"B"},
+  {"text":"Yes","icon":"\u004c","key":"Y"},
+  {"text":"No","icon":"\u004d","key":"N"},
+  {"text":"Thanks","icon":"\u0041","key":"X"},
+  {"text":"Sorry","icon":"\u00a1","key":"S"},
+  {"text":"No Prob","icon":"\u0047","key":"P"},
+  {"text":"Good Game","icon":"\u00a3","key":"G"},
+  {"text":"You","icon":"\u004e","key":"O"},
+  {"text":"Me","icon":"\u004f","key":"E"},
+  {"text":"Mine","icon":"\u0044","key":"M"},
+  {"text":"Attack","icon":"\u0049","key":"A"},
+  {"text":"Help","icon":"\u004a","key":"H"},
+  {"text":"Follow","icon":"\u0050","key":"F"},
+  {"text":"Kill","icon":"\u005b","key":"K"},
+  {"text":"Hmm","icon":"\u004b","key":"Q"}
+]
