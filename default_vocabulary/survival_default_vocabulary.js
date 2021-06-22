1efd3e6657c7d720805bb7de9735dc606ce4f11a
Bhpsngum <bhpsngumproductions@gmail.com>
2021-06-22 17:28:19 +0700

    Update survival_default_vocabulary.js

--- default_vocabulary/survival_default_vocabulary.js
+++ default_vocabulary/survival_default_vocabulary.js
@@ -6,7 +6,7 @@
   {"text":"Thanks","icon":"\u0041","key":"X"},
   {"text":"Sorry","icon":"\u00a1","key":"S"},
   {"text":"No Prob","icon":"\u0047","key":"P"},
-  {"text":"Good Game","icon":"\u00470047","key":"G"},
+  {"text":"Good Game","icon":"\u00a3","key":"G"},
   {"text":"Team","icon":"\u0031","key":"T"},
   {"text":"Follow me","icon":"\u0050","key":"F"},
   {"text":"Mine","icon":"\u0044","key":"M"},
